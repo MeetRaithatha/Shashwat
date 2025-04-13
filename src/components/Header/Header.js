77058aafd4b492eadf5482be7fa8a5dc7bdf7a53
MeetRaithatha <136855296+MeetRaithatha@users.noreply.github.com>
2025-04-13 09:13:31 +0530

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { NAVIGATION_ITEMS } from '../../utils/constants';
 import './Header.css';
-import logo from './Logo.png';
 
 function Header() {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -20,70 +19,47 @@ function Header() {
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
+  const closeMenu = () => {
+    setMobileMenuOpen(false);
+  };
+
   return (
-<header className={`header ${isScrolled ? 'header-scrolled' : ''}`}>
+    <header className={`header ${isScrolled ? 'header-scrolled' : ''}`}>
       <div className="header-container">
         <div className="logo-container">
           <a href="/" className="logo">
-          <img src={logo} alt="Logo" className="logo" />
+            <img src="/logo.png" alt="EcoDevelop Logo" />
           </a>
         </div>
 
-        <div className="header-right">
-          <div className="utility-nav">
-            
-            <nav className="quick-links">
-              <ul>
-                <li><a href="/projects">Projects</a></li>
-                <li><a href="/carbon-calculator">Carbon Calculator</a></li>
-                <li>
-                  <button className="menu-button" onClick={() => setMobileMenuOpen(!mobileMenuOpen)}>
-                    <span className="menu-icon">☰</span>
-                  </button>
-<nav className={`dropdown-menu ${mobileMenuOpen ? 'open' : ''}`}>
-                    <ul>
-                      <li><a href="/sustainability">Sustainability</a></li>
-                      <li><a href="/case-studies">Case Studies</a></li>
-                      <li><a href="/blogs">Blogs</a></li>
-                      <li><a href="/about-us">About Us</a></li>
-                    </ul>
-                  </nav>
-                </li>
-              </ul>
-            </nav>
-          </div>
+        <button 
+          className={`menu-toggle ${mobileMenuOpen ? 'open' : ''}`}
+          onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+          aria-label="Toggle menu"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
 
-          <button 
-            className="mobile-menu-toggle" 
-            onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
-          >
-            {mobileMenuOpen ? 'Close' : 'Menu'}
-          </button>
-
-<nav className={`main-nav ${mobileMenuOpen ? 'mobile-open' : ''}`}>
-            <ul>
-              {NAVIGATION_ITEMS.map((item, index) => (
-                <li key={index} className={item.subItems ? 'dropdown' : ''}>
-                  <a href={item.link}>{item.title}</a>
-                  {item.subItems && (
-                    <div className="dropdown-content">
-                      {item.subItems.map((subItem, subIndex) => (
-                        <a key={subIndex} href={subItem.link}>
-                          {subItem.title}
-                        </a>
-                      ))}
-                    </div>
-                  )}
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </div>
+        <nav className={`main-nav ${mobileMenuOpen ? 'mobile-open' : ''}`}>
+          <ul>
+            {NAVIGATION_ITEMS.map((item, index) => (
+              <li key={index}>
+                <a 
+                  href={item.link} 
+                  onClick={closeMenu}
+                  className="nav-link"
+                >
+                  {item.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
     </header>
   );
 }
 
-export default Header; 
-
-
+export default Header;
